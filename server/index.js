a365c88a4ef69ea127d6408e7c2af417da6de83b
keyurparalkar <keur.plkar@gmail.com>
2024-08-02 07:01:29 +0530

    feat: added API /image endpoint that sends image to the client

--- server/index.js
+++ server/index.js
@@ -8,6 +8,12 @@ app.get("/", function (req, res) {
 	res.end();
 });
 
+app.get("/image", function (req, res) {
+	res.sendFile(
+		"/Users/keyurparalkar/Documents/Projects/embedded-image-preview/server/images/sample_1.jpg"
+	);
+});
+
 app.listen(PORT, function (err) {
 	if (err) console.log(err);
 	console.log("Server listening on PORT", PORT);
